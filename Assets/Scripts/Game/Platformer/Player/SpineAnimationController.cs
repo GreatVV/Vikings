7cc20825cab568f4d5e7ebd08983b0ad8d775185
GreatVV <marinegreatvv@yahoo.com>
2015-01-13 23:30:40 +0300

    Add spine Add simple spine animation controller

---
+++ Assets/Scripts/Game/Platformer/Player/SpineAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpineAnimationController : MonoBehaviour {
+
+    public RaycastCharacterController2D controller;
+
+    public string idleName;
+    public string moveName;
+
+    public SkeletonAnimation SkeletonAnimation;
+
+	// Update is called once per frame
+    void Update()
+    {
+        // When turning face towards the camera, unless climbing/rope climbing
+        if (controller.State == CharacterState.IDLE)
+        {
+            SkeletonAnimation.loop = false;
+            SkeletonAnimation.AnimationName = idleName;
+        }
+
+        if (controller.State == CharacterState.WALKING || controller.State == CharacterState.RUNNING)
+        {
+            SkeletonAnimation.loop = true;
+            SkeletonAnimation.AnimationName = moveName;
+        }
+    }
+}
